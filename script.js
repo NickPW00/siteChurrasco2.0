808e500ce1853ea9f3e487d730de3bc9e62ad9ee
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-04-03 12:26:27 -0300

    Finalizado

--- script.js
+++ script.js
@@ -7,11 +7,39 @@ let validacoes = {
     nome: false,
     email:false,
     telefone: false,
+    radio:false,
     check: false,
-    cpf: false,
-    data: false,
-    endereco: false,
-  }
+    texto: false,
+    selecionar: false
+}
+
+function validar() {
+    validaNome()
+    validacaoEmail()
+    validacaoTelefone()
+    validacaoTexto()
+    validarSelecionar()
+    verificar();
+}
+
+let input = qsA("input")
+
+function verificar() {
+    for(let el in validacoes){
+        if(validacoes[el] == true) {
+        checkValidacao = true
+        } else if(validacoes[el] == false) {
+        checkValidacao = false;
+        alert(`O elemento ${el} está incorreto.`)
+        break;
+        }
+    }
+    if(checkValidacao == true){
+        input.forEach(el => el.value = "") 
+        alert("Você foi Cadastrado.")
+    }
+}
+
 
 let nome = qs("#nome");
 
@@ -25,14 +53,15 @@ let email = qs("#email");
 function validacaoEmail() {
     let chave1 = false
     let chave2 = false
+    let emailArray = email.value.split("")
     let eAiOf = emailArray.indexOf("@")
-    emailArray = email.value.split("")
-    emailArray.forEach(el => {
-        if(el === "@") {
-        emailArray.forEach(le => {if(le === "."){chave1 = true}})
+    for(let el of emailArray) {
+        if(el == "@") {
+        for(let el of emailArray){if(el === "."){chave1 = true}
+            }
         }
-    })
-    if(emailArray.indexOf("@") < emailArray.indexOf(".", eAiOf + 1)){
+    }
+    if(emailArray.indexOf("@") + 2 < emailArray.indexOf(".", "@")){
         chave2 = true
     }
     if(chave1 && chave2){validacoes.email = true}
@@ -41,7 +70,7 @@ function validacaoEmail() {
 let telefone = qs("#telefone")
 
 function validacaoTelefone() {
-    let telefoneArray = telefone.value.replaceAll(/[()-]/g, "").split("");
+    let telefoneArray = telefone.value.replaceAll(/[()-]/g, "").replaceAll(" ", "").split("");
     seEntao(telefoneArray.length === 11, "telefone")
 }
 
@@ -61,7 +90,7 @@ function marcar() {
     event.target.classList.toggle("marcado");
     for(let a of radioC3){
         if(a.classList.contains("marcado")){
-            validacoes.check = true
+            validacoes.radio = true
         }
     }
 };
@@ -77,23 +106,26 @@ function marcarTodos() {
     if(meios.classList.contains("marcado")){
         meios.classList.toggle("marcado")
         check.forEach(el => {el.classList.remove("marcado");})
+        validacoes.check = false;
     }
     else if(meios.classList.contains("marcadoMetade")){
         meios.classList.remove("marcadoMetade")
         check.forEach(el => {el.classList.remove("marcado");})
+        validacoes.check = false;
     }
     else{
         meios.classList.toggle("marcado")
         check.forEach(el => {el.classList.toggle("marcado");})
+        validacoes.check = true;
     }
 }; 
 
 let todosMarcados = false
 let nenhumMarcado = true
 
-check.forEach(a => a.onclick = marcar) 
+check.forEach(a => a.onclick = marcarCheck) 
 
-function marcar() {
+function marcarCheck() {
     if(event.target.classList.contains("marcado")){
         event.target.classList.remove("marcado");
         checagemDeTodos()
@@ -120,6 +152,7 @@ function checagemDeTodos(){
     for(let el of check){
         if(el.classList.contains("marcado")){
             todosMarcados = true
+            validacoes.check = true;
         }
         else{
             todosMarcados = false; break;
@@ -127,10 +160,40 @@ function checagemDeTodos(){
     }
     for(let el of check){
         if(!el.classList.contains("marcado")){
-            nenhumMarcado = true
+            nenhumMarcado = true; validacoes.check = false;
         }
         else{
-            nenhumMarcado = false; break;
+            nenhumMarcado = false;
+            validacoes.check = true;
+            break;
         }
+    }
+}
+
+let texto = qs("#texto")
+
+function validacaoTexto() {
+    let textoArray = texto.value.split("")
+    seEntao(textoArray.length > 5, "texto");
+}
+
+let select = qs("#select")
+let selectBox = qs("#selectBox")
+let selectBoxFilhos = qsA("#selectBox div")
+
+function selecionar() {
+    selectBox.classList.toggle("hidden");
+    selectBoxFilhos.forEach(el => {
+        el.onclick = _ => {
+            select.innerHTML = event.target.innerHTML;
+            selectBox.classList.toggle("hidden");
+    }})
+}
+
+select.onclick = selecionar
+
+function validarSelecionar () {
+    if(select.innerHTML != "Selecione"){
+        validacoes.selecionar = true
     }
 }
